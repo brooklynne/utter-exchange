4bca78e8073332b19122614f44dbac7a9f525348
Kord Campbell <kordless@gmail.com>
2014-09-08 20:44:22 -0700

    install fix

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
-me: install.sh
+#!/bin/bash
+
+# name: install.sh
 # description: install script for utter.io prezo
 # author: kordless@utter.io
 
@@ -11,4 +13,3 @@ git clone https://github.com/StackMonkey/utter-exchange.git
 # move into prezo and start webserver
 cd utter-exchange/static/prezo/
 python -m SimpleHTTPServer 80 
-
